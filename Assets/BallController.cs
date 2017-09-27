329f6accd94c0d5778eacdb483654628d3329b00
lepakki <lepackki@gmail.com>
2017-09-27 18:21:28 +0300

    initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Controller ctrl;
+
+    public float force = 1000;
+    private Light lt;
+    private float newRed, newGreen, newBlue, newRed2, newGreen2, newBlue2;
+
+	// Use this for initialization
+	void Start () {
+
+        lt = GetComponent<Light>();
+        rb = GetComponent<Rigidbody>();
+        ctrl = FindObjectOfType<Controller>();
+
+        RandomizeLightLevel();
+        RandomizeColor();
+
+        Vector3 sp = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = (Input.mousePosition - sp).normalized;
+
+        force = 1700;
+        rb.AddForce(-ctrl.firingArc * force);
+        Destroy(gameObject, 3f);
+
+    }
+
+    void RandomizeColor()
+    {
+        newRed2 = Random.Range(0, 0.9f);
+        newGreen2 = Random.Range(0, 0.9f);
+        newBlue2 = Random.Range(0, 0.9f);
+        gameObject.GetComponent<Renderer>().material.color = new Color(newRed2, newGreen2, newBlue2);
+    }
+
+    void RandomizeLightLevel()
+    {
+        newRed = Random.Range(0, 255);
+        newGreen = Random.Range(0, 255);
+        newBlue = Random.Range(0, 255);
+        lt.color = new Color(newRed, newGreen, newBlue, 255);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(Vector3.right * Time.deltaTime * 5);
+	}
+}
---
+++ Assets/Controller.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+
+
+    public Text ballsFiredText;
+    public Text ballSpeedText;
+    private int ballsFiredCount = 0;
+
+    public Button quicker, slower;
+
+    public GameObject ball;
+    public Vector3 firingArc;
+    private Quaternion origin;
+    private Vector3 mouse_pos;
+    private Vector3 player_pos;
+    private Vector3 camera_pos;
+
+    private float firingCooldown = 0.1f;
+    private bool onCooldown;
+    
+
+
+	// Use this for initialization
+	void Start () {
+        origin = new Quaternion(transform.position.x, transform.position.y, transform.position.z, 1);
+        camera_pos = Camera.main.transform.position;
+        ballSpeedText.text = "Speed : " + firingCooldown;
+
+	}
+
+    private void Update()
+    {
+        mouse_pos = Input.mousePosition;
+        mouse_pos.z = 5;
+        player_pos = Camera.main.ScreenToWorldPoint(mouse_pos);
+        firingArc = camera_pos - player_pos;
+        Debug.DrawLine(camera_pos, player_pos, Color.cyan);
+
+        
+        if (Input.GetKey(KeyCode.Mouse0) && !onCooldown)
+             {
+               Shoot();
+               onCooldown = true;
+           }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (firingCooldown < 1.5f)
+            {
+                firingCooldown *= 1.3f;
+                ballSpeedText.text = "Speed : " + firingCooldown.ToString("F5");
+            } else
+            {
+                firingCooldown = 1.5f;
+                ballSpeedText.text = "Speed : " + firingCooldown.ToString("F5");
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (firingCooldown > 0.00001f)
+            {
+                firingCooldown *= 0.7f;
+                ballSpeedText.text = "Speed : " + firingCooldown.ToString("F5");
+            } else
+            {
+                firingCooldown = 0.00001f;
+                ballSpeedText.text = "Speed : " + firingCooldown.ToString("F5");
+            }
+        }
+    }
+
+
+    void Shoot()
+    {
+        Instantiate(ball, player_pos, origin);
+        ballsFiredCount++;
+        if (ballsFiredCount < 1000)
+        {
+            ballsFiredText.text = "Balls Fired : " + ballsFiredCount;
+        } else if (ballsFiredCount > 1000 && ballsFiredCount < 2000)
+        {
+            ballsFiredText.text = "Balls Fired : Too many.";
+        } else
+        {
+            ballsFiredText.text = "Balls Fired : " + ballsFiredCount + " ..";
+        }
+        StartCoroutine(coolDown(firingCooldown));
+    }
+
+    IEnumerator coolDown(float firingCooldown)
+    {
+        yield return new WaitForSeconds(firingCooldown);
+        onCooldown = false;
+    }
+}
